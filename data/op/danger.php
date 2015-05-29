d3c06c0f03dbb0d1e1485d26003e807e7bfb5646
gaël huot <ghuot90@gmail.com>
2015-05-29 09:47:18 +0200

    ajout du modal danger + info

--- data/op/danger.php
+++ data/op/danger.php
@@ -1,3 +1,10 @@
 <?php
 	include('../data/connexion_bdd.php');
+	$req_danger = "SELECT EMPRUNT.idEmprunt,ADHERENT.nomAdherent,DATEDIFF(CURRENT_DATE,dateEmprunt) as nb_jours FROM EMPRUNT,ADHERENT WHERE  ADHERENT.idAdherent = EMPRUNT.idAdherent and DATEDIFF(CURRENT_DATE,dateEmprunt)>30 and dateRendu is NULL GROUP BY idEmprunt;";
+	$req_danger = $pdo->query($req_danger);
+	$rep_danger = $req_danger->fetchAll();
+
+	foreach ($rep_danger as $key) {
+		echo 'L\'adherent <span style="color:red;">'.$key['nomAdherent']."</span> possède un emprunt depuis <span style='color:red;'>".$key['nb_jours']."</span> jours.<br/>";
+	}
 ?>
